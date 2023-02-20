981f79575ba2a05d5e55ced3158f4f73e7c52dfa
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-02-20 16:45:22 +0900

    Docs: 로그인 기능 구현

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -3,11 +3,15 @@ import Home from "../routes/Home";
 import Detail from "../routes/Detail";
 import WriteTodo from "../routes/WriteDiary";
 import ModifyDiary from "../routes/ModifyDiary";
+import Login from "../routes/Login";
+import Reigistor from "../routes/Reigistor";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/registor" element={<Reigistor />} />
         <Route path="/" element={<Home />}></Route>
         <Route path="/WriteTodo" element={<WriteTodo />}></Route>
         <Route path="/:id" element={<ModifyDiary />}></Route>
